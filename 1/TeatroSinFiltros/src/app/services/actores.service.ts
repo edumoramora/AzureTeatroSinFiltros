8e2ed7bf2cebdbb64605475224eeb4bf09e06ed5
edumoramora <67929314+edumoramora@users.noreply.github.com>
2024-03-27 13:01:22 +0100

    Update actores.service.ts

--- 1/TeatroSinFiltros/src/app/services/actores.service.ts
+++ 1/TeatroSinFiltros/src/app/services/actores.service.ts
@@ -7,12 +7,18 @@ import { Observable } from 'rxjs';
 })
 
 export class ActoresService {
-  private apiUrl = 'http://localhost:3000/actores';
+ private apiUrl = 'https://dqpcisxtwsasxfdtqdwd.supabase.co/rest/v1/actores';
+ private apikey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxcGNpc3h0d3Nhc3hmZHRxZHdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE0NzQ3MDEsImV4cCI6MjAyNzA1MDcwMX0.ZTJGt2t6xTEP2QZCdkR6qjgRkGnUhkqtD_xzlKFO_6s';
+
 
   constructor(private http: HttpClient) { }
 
-  getActores(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}`);
+ getActores(): Observable<any[]> {
+    const headers = new HttpHeaders()
+      .set('apikey', this.apikey)
+      .set('Authorization', `Bearer ${this.apikey}`);
+
+    return this.http.get<any[]>(this.apiUrl + '?select=*', { headers });
   }
 
   deleteActor(actorId: number): Observable<any> {
