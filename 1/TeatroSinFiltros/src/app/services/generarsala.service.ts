a5bcbd58d0b2d49209cdc4a95b4d3e4c89b99567
edumoramora <67929314+edumoramora@users.noreply.github.com>
2024-04-04 14:01:15 +0200

    Update generarsala.service.ts

--- 1/TeatroSinFiltros/src/app/services/generarsala.service.ts
+++ 1/TeatroSinFiltros/src/app/services/generarsala.service.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
 
 export class GenerarsalaService {
     private apiUrl = 'https://dqpcisxtwsasxfdtqdwd.supabase.co/rest/v1/salas';
+    private apikey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxcGNpc3h0d3Nhc3hmZHRxZHdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE0NzQ3MDEsImV4cCI6MjAyNzA1MDcwMX0.ZTJGt2t6xTEP2QZCdkR6qjgRkGnUhkqtD_xzlKFO_6s';
 
   constructor(private http: HttpClient) { }
 
@@ -24,7 +25,7 @@ export class GenerarsalaService {
       'Content-Type': 'application/json'
     });
 
-    return this.http.get<any[]>(this.apiUrl, { headers });
+    return this.http.get<any[]>(this.apiUrl + '?select=*', { headers });
   }
 
   eliminarSala(id: number): Observable<any> {
