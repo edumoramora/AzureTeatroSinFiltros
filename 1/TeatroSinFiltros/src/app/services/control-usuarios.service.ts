5023f41cf575fc7a92f5a60f75afbb3a03e8234c
edumoramora <67929314+edumoramora@users.noreply.github.com>
2024-04-05 11:55:30 +0200

    Update control-usuarios.service.ts

--- 1/TeatroSinFiltros/src/app/services/control-usuarios.service.ts
+++ 1/TeatroSinFiltros/src/app/services/control-usuarios.service.ts
@@ -7,9 +7,9 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ControlUsuariosService {
-  private apiUrl = 'https://azure-teatro-sin-filtros-k7nf-edumoramoras-projects.vercel.app/api/usuarios'; 
+   private apiUrl = 'https://dqpcisxtwsasxfdtqdwd.supabase.co/rest/v1/usuarios'; 
   private apikey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxcGNpc3h0d3Nhc3hmZHRxZHdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE0NzQ3MDEsImV4cCI6MjAyNzA1MDcwMX0.ZTJGt2t6xTEP2QZCdkR6qjgRkGnUhkqtD_xzlKFO_6s';
-
+  
   constructor(private http: HttpClient) { }
   
 obtenerUsuarios(): Observable<any[]> {
